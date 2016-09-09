f5e3dcdfe5c4d8db307774397abe7a8754ae5213
Milan Flach <mflach@bgc-jena.mpg.de>
2016-09-09 21:58:09 +0200

    minor changes

--- src/DetectionAlgorithms.jl
+++ src/DetectionAlgorithms.jl
@@ -6,6 +6,8 @@ using LIBSVM
     REC(D::AbstractArray, rec_threshold::Float64, temp_excl::Int = 5)
 
 Count the number of observations (recurrences) which fall into a radius `rec_threshold` of a distance matrix `D`. Exclude steps which are closer than `temp_excl` to be count as recurrences (default: `temp_excl = 5`)
+
+Marwan, N., Carmen Romano, M., Thiel, M., & Kurths, J. (2007). Recurrence plots for the analysis of complex systems. Physics Reports, 438(5-6), 237–329. http://doi.org/10.1016/j.physrep.2006.11.001
 """
 
 function REC(D::AbstractArray, rec_threshold::Float64, temp_excl::Int = 5)
@@ -19,8 +21,6 @@ end
     init_REC(T::Int)
 
 get object for memory efficient `REC!()` versions. Input can be a distance matrix `D` or the number of timesteps (observations) `T`.
-
-Marwan, N., Carmen Romano, M., Thiel, M., & Kurths, J. (2007). Recurrence plots for the analysis of complex systems. Physics Reports, 438(5-6), 237–329. http://doi.org/10.1016/j.physrep.2006.11.001
 """
 
 function init_REC(T::Int)
--- src/FeatureExtraction.jl
+++ src/FeatureExtraction.jl
@@ -259,7 +259,7 @@ end
 """
     EWMA(dat,  λ)
 
-Compute the exponential weighted moving average (EWMA) with the weighting parameter `λ` between 0 (full weighting) and 1 (no weighting) in the first dimension of `dat`.
+Compute the exponential weighted moving average (EWMA) with the weighting parameter `λ` between 0 (full weighting) and 1 (no weighting) along the first dimension of `dat`.
 Supports N-dimensional Arrays.
 
 Lowry, C. A., & Woodall, W. H. (1992). A Multivariate Exponentially Weighted Moving Average Control Chart. Technometrics, 34, 46–53.
