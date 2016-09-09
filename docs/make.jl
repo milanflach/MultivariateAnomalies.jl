d8b8521e2d2165c8461a76eebbe56eb33929019f
Milan Flach <mflach@bgc-jena.mpg.de>
2016-09-09 11:40:48 +0200

    mkdocs again

--- docs/make.jl
+++ docs/make.jl
@@ -3,18 +3,18 @@
 using Documenter, MultivariateAnomalies
 
 makedocs(modules = [MultivariateAnomalies],
-         format   = Documenter.Formats.HTML,
-         sitename = "MultivariateAnomalies.jl",
-             pages    = Any[
-           "Home" =>  "index.md",
-           "Manual" => Any[
-               "man/FeatureExtraction.md"
-             , "man/DetectionAlgorithms.md"
-             , "man/AUC.md"
-             , "man/DistDensity.md"
-             , "man/Scores.md"
-             ]
-           ]
+         #format   = Documenter.Formats.HTML,
+         #sitename = "MultivariateAnomalies.jl",
+         #pages    = Any[
+#            "Home" =>  "index.md",
+#            "Manual" => Any[
+#                "man/FeatureExtraction.md"
+#              , "man/DetectionAlgorithms.md"
+#              , "man/AUC.md"
+#              , "man/DistDensity.md"
+#              , "man/Scores.md"
+#              ]
+#            ]
          )
 
 
@@ -22,7 +22,7 @@ deploydocs(
   repo = "github.com/milanflach/MultivariateAnomalies.jl.git",
   julia = "release",
   osname = "osx",
-  target = "build", # not needed for mkdocs
-  make = nothing, # not needed for mkdocs
-  deps = nothing #Deps.pip("mkdocs", "python-markdown-math"), # change for mkdocs
+  #target = "build", # not needed for mkdocs
+  #make = nothing, # not needed for mkdocs
+  deps = Deps.pip("mkdocs", "python-markdown-math") #nothing, # change for mkdocs
 )
