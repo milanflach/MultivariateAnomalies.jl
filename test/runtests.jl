4aac452f8b9f1cd9fb413e8c7891c4720a0f2274
Milan Flach <mflach@bgc-jena.mpg.de>
2016-11-21 15:02:17 +0100

    runtests after changes correct

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,13 +18,13 @@ K = kernel_matrix(D, sigma)
 svdd_model = SVDD_train(K[1:4,1:4], 0.2);
 knfst_model = KNFST_train(K[1:4,1:4])
 # REC, checked manually
-@test all(REC(D, sigma, 0) .== [1,0,2,1,2])
+@test all(round(REC(D, sigma, 0), 1) .== round(1 .- ([1,0,2,1,2] ./ 5), 1))
 # KNN Gamma, checked manually
 @test all(round(KNN_Gamma(knn_dists_out), 1) .== [1.6, 2.1,1.2,1.6,1.2])
 # KNN Delta, approximately
 @test all(round(KNN_Delta(knn_dists_out, dat), 1) .== [1.4, 2.1, 0.5, 1.6, 0.5])
 # KDE # results show exhibit similar ordering like REC
-@test all(sortperm(-KDE(K)) .== [5,3,1,4,2])
+@test all(sortperm(KDE(K)) .== [5,3,1,4,2])
 # Hotelling's T^2
 # is also the quared mahalanobis distance to the data's mean
 using Distances
@@ -45,8 +45,8 @@ P.Q = Q
 # detectAnomalies
 detectAnomalies(dat, P)
 # chekct detectAnomalies for self consistency
-@test round(P.REC, 3) == round(-REC(D, sigma, 0), 3)
-@test round(P.KDE, 3) == round(-KDE(K), 3)
+@test round(P.REC, 3) == round(REC(D, sigma, 0), 3)
+@test round(P.KDE, 3) == round(KDE(K), 3)
 @test round(P.KNN_Gamma, 3) ==  round(KNN_Gamma(knn_dists_out), 3)
 @test round(P.KNN_Delta[1], 3) ==  round(KNN_Delta(knn_dists_out, dat), 3)
 @test round(P.T2[1], 3) ==  round(T2(dat, Q, mean(dat, 1)),3)
@@ -62,4 +62,3 @@ detectAnomalies(dat, P)
 @test K[1:4,1:4] == P.D_train[1]
 @test Ktest == P.D_test[1]
 @test round(P.KNFST[1], 3) == round(KNFST_predict(knfst_model, Ktest)[1], 3)
-
