7bb2a1344e9ddd0dcd83d91641b97f1b20a7999b
Milan Flach <mflach@bgc-jena.mpg.de>
2017-02-06 11:20:06 +0100

    docs ..

--- docs/make.jl
+++ docs/make.jl
@@ -23,6 +23,7 @@ deploydocs(
   deps = Deps.pip("mkdocs", "python-markdown-math") #nothing # change to nothing for mkdocs
   , repo = "github.com/milanflach/MultivariateAnomalies.jl.git"
   , julia = "release"
+  
   #, osname = "osx"
   #, target = "build" # not needed for mkdocs
   #, make = nothing # not needed for mkdocs
